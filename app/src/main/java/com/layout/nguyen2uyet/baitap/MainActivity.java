968edc87b0f61df23732a318190136a90f2e992a
unknown <nguyen2uyet@gmail.com>
2018-06-01 18:56:05 +0300

    Initial commit

---
+++ app/src/main/java/com/layout/nguyen2uyet/baitap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.layout.nguyen2uyet.baitap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
